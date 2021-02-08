c6ab8b87c7866455e9048c6ceec663c2358c826e
Alexey <alexey-lopatenko@yandex.ru>
2021-02-08 19:03:42 +0300

    Провел рефакторинг кода. Вынес все управления Material Navigation Drawer в отдельный класс AppDrawer. Создал общий базовый фрагмент для всех фрагментов приложения, использовал наследование, задал общее поведение всех фрагментов.

---
+++ app/src/main/java/android/example/telegram/ui/fragments/ChatsFragment.kt
@@ -0,0 +1,13 @@
+package android.example.telegram.ui.fragments
+
+import android.example.telegram.R
+import android.example.telegram.databinding.FragmentChatsBinding
+
+class ChatsFragment : BaseFragment(R.layout.fragment_chats) {
+
+    private lateinit var mBinding: FragmentChatsBinding
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
---
+++ app/src/main/java/android/example/telegram/ui/fragments/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package android.example.telegram.ui.fragments
+
+import android.example.telegram.R
+
+
+class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
