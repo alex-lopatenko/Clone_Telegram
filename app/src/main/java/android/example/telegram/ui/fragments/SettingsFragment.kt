0a2536d3adb848976c202d5fe28020c842d0a98e
Alexey <alexey-lopatenko@yandex.ru>
2021-02-09 16:19:57 +0300

    Добавил выпадающее меню с иконками

--- app/src/main/java/android/example/telegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/android/example/telegram/ui/fragments/SettingsFragment.kt
@@ -1,11 +1,18 @@
 package android.example.telegram.ui.fragments
 
 import android.example.telegram.R
+import android.view.Menu
+import android.view.MenuInflater
 
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
     }
 }
