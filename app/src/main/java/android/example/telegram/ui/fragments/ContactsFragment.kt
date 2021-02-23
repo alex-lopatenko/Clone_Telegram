1ae52625fdd1efcb61ad4a536b466c0586edf5ca
Alexey <alexey-lopatenko@yandex.ru>
2021-02-23 16:49:59 +0300

    Устранил утечку памяти в приложении.

--- app/src/main/java/android/example/telegram/ui/fragments/ContactsFragment.kt
+++ app/src/main/java/android/example/telegram/ui/fragments/ContactsFragment.kt
@@ -22,7 +22,9 @@ class ContactsFragment : BaseFragment(R.layout.fragment_contacts) {
     private lateinit var mRecyclerView: RecyclerView
     private lateinit var mAdapter: FirebaseRecyclerAdapter<CommonModel, ContactsHolder>
     private lateinit var mRefContacts: DatabaseReference
-    private lateinit var mRefUsers:DatabaseReference
+    private lateinit var mRefUsers: DatabaseReference
+    private lateinit var mRefUsersListener: AppValueEventListener
+    private var mapListeners = hashMapOf<DatabaseReference, AppValueEventListener>()
 
     override fun onResume() {
         super.onResume()
@@ -37,10 +39,11 @@ class ContactsFragment : BaseFragment(R.layout.fragment_contacts) {
         val options = FirebaseRecyclerOptions.Builder<CommonModel>()
             .setQuery(mRefContacts, CommonModel::class.java)
             .build()
-        mAdapter = object :FirebaseRecyclerAdapter<CommonModel, ContactsHolder>(options) {
+        mAdapter = object : FirebaseRecyclerAdapter<CommonModel, ContactsHolder>(options) {
 
             override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsHolder {
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.contact_item, parent, false)
                 return ContactsHolder(view)
             }
 
@@ -50,14 +53,16 @@ class ContactsFragment : BaseFragment(R.layout.fragment_contacts) {
                 model: CommonModel
             ) {
                 mRefUsers = REF_DATABASE_ROOT.child(NODE_USERS).child(model.id)
-                mRefUsers.addValueEventListener(AppValueEventListener{
 
+                mRefUsersListener = AppValueEventListener {
                     val contact = it.getCommonModel()
-
                     holder.name.text = contact.fullname
                     holder.status.text = contact.state
                     holder.photo.downloadAndSettImage(contact.photoUrl)
-                })
+                }
+
+                mRefUsers.addValueEventListener(mRefUsersListener)
+                mapListeners[mRefUsers] = mRefUsersListener
             }
 
         }
@@ -75,6 +80,9 @@ class ContactsFragment : BaseFragment(R.layout.fragment_contacts) {
     override fun onPause() {
         super.onPause()
         mAdapter.stopListening()
+        mapListeners.forEach {
+            it.key.removeEventListener(it.value)
+        }
     }
 }
 
